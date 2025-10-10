d2717d2cbc9a6f84c5566eb49aaeb4f42581a7bc
soomro-abd <msoomro@umass.edu>
2025-10-10 16:35:02 +0000

    Deploying to gh-pages from @ soomro-abd/soomro-abd.github.io@c98e252ec6f37933c743862ecbd31083d7b5d7a6 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "soomro-abd/.github.io",
+    "data-repo": "soomro-abd/soomro-abd.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
